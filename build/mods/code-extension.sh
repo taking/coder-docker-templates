8e875893d0feb72e8ec4fb07d687d823e5a2d222
taking <taking@duck.com>
2023-10-04 09:01:36 +0000

    updated

--- build/mods/code-extension.sh
+++ build/mods/code-extension.sh
@@ -11,3 +11,79 @@ curl -L "https://github.com/taking/coder-docker-templates/releases/download/v0.1
 /tmp/code-server/bin/code-server --install-extension /tmp/extensions/seatonjiang.gitmoji-vscode-1.2.4.vsix
 /tmp/code-server/bin/code-server --install-extension /tmp/extensions/tal7aouy.theme-3.1.0.vsix
 /tmp/code-server/bin/code-server --install-extension /tmp/extensions/vscode-icons-team.vscode-icons-12.5.0.vsix
+
+cat <<EOF > $HOME/.local/share/code-server/User/settings.json
+{
+  "editor.fontFamily": "Hack, D2Coding, Consolas, Menlo, Monaco, 'Courier New', monospace",
+  "editor.fontSize": 17,
+  "editor.tabSize": 2,
+  "editor.wordWrap": "on",
+  "editor.wrappingIndent": "indent",
+  "editor.fontLigatures": true,
+  "editor.rulers": [100, 120],
+  "editor.cursorBlinking": "phase",
+  "editor.guides.indentation": true,
+  "editor.codeActionsOnSave": {
+  
+  },
+  "editor.lineHeight" : 26,
+  "editor.suggestSelection" : "first",
+  "editor.renderLineHighlight" : "gutter",
+  "editor.suggest.showStatusBar": true,
+  "files.eol": "\n",
+  "terminal.integrated.fontSize": 18,
+  "ActiveFileInStatusBar.enable": true,
+  "ActiveFileInStatusBar.fullpath": true,
+  "ActiveFileInStatusBar.revealFile": false,
+  
+  // Theme & Icon Start
+  "workbench.iconTheme": "vscode-icons",
+  "workbench.colorTheme": "Theme"
+  "workbench.statusBar.visible": true,
+  "workbench.activityBar.visible": true,
+  "vsicons.dontShowNewVersionMessage": true,  // vscode-icons
+  // Theme & Icon Stop
+
+  // "editor.formatOnSave": false,
+
+  // auto-close-tag Start
+  "auto-close-tag.SublimeText3Mode": true,
+  "auto-close-tag.activationOnLanguage": [
+      "html",
+      "xml",
+      "php",
+      "css",        
+      "javascript",
+      "js",
+      "vue"
+  ],
+  // auto-close-tag End
+
+  // indentRainbow Start
+  "indentRainbow.ignoreErrorLanguages" : [
+    "markdown"
+  ],
+  // indentRainbow End
+
+  // Gitmoji Start
+  "gitmoji.additionalEmojis": [
+    {
+      "emoji": "🐛",
+      "code": ":bug:",
+      "description": "Fix a bug.",
+      "description_ko_kr": "BUG 수정","remote.SSH.remotePlatform": {"coder-vscode--taking--nw":"linux","coder-vscode--taking--gedge--main":"linux","coder-vscode--taking--nw--main":"linux"}
+    },
+    {
+      "emoji": "🚑",
+      "code": ":ambulance:",
+      "description": "Critical hotfix.",
+      "description_zh_cn": "크리티컬 긴급수정"
+    }
+  ],
+  "remote.SSH.remotePlatform": {"coder-vscode--taking--nw--main":"linux","coder-vscode--taking--nw":"linux","coder-vscode--taking--java":"linux"},
+  "terminal.integrated.env.osx": {
+    "FIG_NEW_SESSION": "1"
+  }
+  // Gitmoji End
+}
+EOF
