ea05a292f9367494389a95d5b6ebc3d3b066ba6a
taking <taking@duck.com>
2023-10-04 09:13:13 +0000

    updated

--- main.tf
+++ main.tf
@@ -125,7 +125,7 @@ resource "coder_app" "code-server" {
   agent_id     = coder_agent.main.id
   slug         = "code-server"
   display_name = "code-server"
-  url          = "http://localhost:13337/?folder=/home/${local.username}"
+  url          = "http://localhost:13337/?folder=/home/${local.username}/workspaces"
   icon         = "/icon/code.svg"
   subdomain    = false
   share        = "owner"
