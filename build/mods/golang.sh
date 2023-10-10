00711d679ff2f3a965121a213d52ea9a8e15d1c9
taking <taking@duck.com>
2023-10-10 04:00:08 +0000

    updated

--- build/mods/golang.sh
+++ build/mods/golang.sh
@@ -6,17 +6,18 @@ if [ -d "$go_folder" ]; then
   echo "go Exists"
 else
   echo "go Installing"
-  curl -L "https://go.dev/dl/go1.21.1.linux-amd64.tar.gz" | tar -C /usr/local -xzvf -
+  curl -L "https://go.dev/dl/go1.21.2.linux-amd64.tar.gz" | sudo tar -C /usr/local -xzvf -
 
   cat <<'EOF' >> ~/.zshrc
 export GO_ROOT=/usr/local/go
-export PATH=$PATH:$GOROOT/bin
-export GOPATH=$HOME/go
-export GOBIN=$GOPATH/bin
-export PATH=$PATH:$GOBIN
+export PATH=$PATH:$GO_ROOT/bin
+export GO_PATH=$HOME/go
+export GO_BIN=$GO_PATH/bin
+export PATH=$PATH:$GO_BIN
 EOF
 
-  echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | tee /etc/apt/sources.list.d/goreleaser.list
-  apt-get update && apt-get install goreleaser -y
+  echo 'deb [trusted=yes] https://repo.goreleaser.com/apt/ /' | sudo tee /etc/apt/sources.list.d/goreleaser.list
+  sudo apt-get update && sudo apt-get install goreleaser -y
 
 fi
+
--- build/mods/java.sh
+++ build/mods/java.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-jdk_folder="/usr/lib/jdk-17.0.7"
+jdk_folder="/usr/lib/jdk-17.0.7+7"
 
 if [ -d "$jdk_folder" ]; then
   echo "java Exists"
 else
   echo "java Installing"
-  wget https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.7%2B7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.7_7.tar.gz
-  tar xvzf OpenJDK17U-jdk_x64_linux_hotspot_17.0.7_7.tar.gz
-  sudo chown -R root:root jdk-17.0.7+7
-  sudo mv jdk-17.0.7+7/ /usr/lib/jdk-17.0.7
-  rm OpenJDK17U-jdk_x64_linux_hotspot_17.0.7_7.tar.gz
-
+  curl -L "https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.7%2B7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.7_7.tar.gz" | sudo tar -C /usr/lib -xzvf -
 
   cat <<'EOF' >> ~/.zshrc
-export JAVA_HOME=/usr/lib/jdk-17.0.7
+export JAVA_HOME=/usr/lib/jdk-17.0.7+7
 export PATH=$JAVA_HOME/bin/:$PATH
 export CLASS_PATH=$JAVA_HOME/lib:$CLASS_PATH
 EOF
