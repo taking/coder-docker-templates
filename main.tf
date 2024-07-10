87b342a5182a49086da1c76fd2fcb84923a8f6b0
taking <taking@duck.com>
2024-07-10 15:57:25 +0900

    [240710] kuernetes remote template updated

--- main.tf
+++ main.tf
@@ -1,74 +1,165 @@
 terraform {
   required_providers {
     coder = {
-      source = "coder/coder"
+      source  = "coder/coder"
     }
-    docker = {
-      source = "kreuzwerker/docker"
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
     }
   }
 }
 
 locals {
-  username = data.coder_workspace.me.owner
+  folder_name = try(element(split("/", data.coder_parameter.repo.value), length(split("/", data.coder_parameter.repo.value)) - 1), "")  
+  repo_owner_name = try(element(split("/", data.coder_parameter.repo.value), length(split("/", data.coder_parameter.repo.value)) - 2), "")
 }
 
-data "coder_provisioner" "me" {
+provider "coder" {
 }
 
-provider "docker" {
+variable "host" {
+  description = "Cluster host address"
+  sensitive   = true
 }
 
-data "coder_workspace" "me" {
+variable "cluster_ca_certificate" {
+  description = "Cluster CA certificate (base64 encoded)"
+  sensitive   = true
 }
+
+variable "token" {
+  description = "Cluster CA token (base64 encoded)"
+  sensitive   = true
+}
+
+variable "namespace" {
+  description = "Namespace"
+}
+
+
+data "coder_parameter" "cpu" {
+  name         = "cpu"
+  display_name = "CPU"
+  description  = "The number of CPU cores"
+  default      = "2"
+  icon         = "/icon/memory.svg"
+  mutable      = true
+  option {
+    name  = "2 Cores"
+    value = "2"
+  }
+  option {
+    name  = "4 Cores"
+    value = "4"
+  }
+  option {
+    name  = "6 Cores"
+    value = "6"
+  }
+  option {
+    name  = "8 Cores"
+    value = "8"
+  }
+}
+
+data "coder_parameter" "memory" {
+  name         = "memory"
+  display_name = "Memory"
+  description  = "The amount of memory in GB"
+  default      = "2"
+  icon         = "/icon/memory.svg"
+  mutable      = true
+  option {
+    name  = "2 GB"
+    value = "2"
+  }
+  option {
+    name  = "4 GB"
+    value = "4"
+  }
+  option {
+    name  = "6 GB"
+    value = "6"
+  }
+  option {
+    name  = "8 GB"
+    value = "8"
+  }
+}
+
+data "coder_parameter" "home_disk_size" {
+  name         = "home_disk_size"
+  display_name = "Home disk size"
+  description  = "The size of the home disk in GB"
+  default      = "10"
+  type         = "number"
+  icon         = "/emojis/1f4be.png"
+  mutable      = false
+  validation {
+    min = 1
+    max = 99999
+  }
+}
+
+data "coder_parameter" "repo" {
+  name        = "Source Code Repository (optional)"
+  display_name= "Source Code Repository (optional)"
+  description = "What source code repository do you want to clone?"
+  default     = "https://github.com/taking/java-spring-base-structure"
+  type        = "string"
+  icon        = "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
+  mutable     = true
+}
+
+data "coder_parameter" "language" {
+  name        = "Use Language (Optional)"
+  display_name= "Use Language (Optional)"
+  description = "What do you use to language?"
+  default     = "python"
+  type        = "string"
+  icon        = "https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png"
+  mutable     = true  
+  option {
+    name  = "python"
+    value = "python"
+  }
+  option {
+    name  = "go (latest)"
+    value = "go"
+  }
+  option {
+    name  = "rust (latest)"
+    value = "rust"
+  }
+  option {
+    name  = "java (Temurin OpenJDK 21)"
+    value = "java"
+  }
+  option {
+    name  = "node (20.15.1, LTS)"
+    value = "node"
+  }
+}
+
+provider "kubernetes" {
+  host                   = var.host
+  cluster_ca_certificate = base64decode(var.cluster_ca_certificate)
+  token                  = base64decode(var.token)
+}
+
+data "coder_workspace" "me" {}
+data "coder_workspace_owner" "me" {}
 
 resource "coder_agent" "main" {
-  arch                   = data.coder_provisioner.me.arch
   os                     = "linux"
+  arch                   = "amd64"
+  # startup_script         = <<-EOT
+  #   set -e
 
-  display_apps {
-    vscode          = true
-    vscode_insiders = false
-    web_terminal    = true
-    ssh_helper      = false
-    port_forwarding_helper = true
-  }
-
-  startup_script_timeout = 180
-  startup_script         = <<-EOT
-    set -e
-
-    # install and start code-server
-    curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.11.0
-    /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
-
-    # install oh-my-zsh
-    /home/${data.coder_workspace.me.owner}/.mods/oh-my-zsh.sh
-
-    # install java
-    #/home/${data.coder_workspace.me.owner}/.mods/java.sh
-
-    # install vs-code-extensions
-    /home/${data.coder_workspace.me.owner}/.mods/code-extension.sh
-
-    # install node
-    /home/${data.coder_workspace.me.owner}/.mods/node.sh
-
-    # install golang
-    #/home/${data.coder_workspace.me.owner}/.mods/golang.sh
-
-  EOT
-
-  # These environment variables allow you to make Git commits right away after creating a
-  # workspace. Note that they take precedence over configuration defined in ~/.gitconfig!
-  # You can remove this block if you'd prefer to configure Git manually or using
-  # dotfiles. (see docs/dotfiles.md)
-  env = {
-    GIT_AUTHOR_NAME     = "${data.coder_workspace.me.owner}"
-    GIT_COMMITTER_NAME  = "${data.coder_workspace.me.owner}"
-    GIT_AUTHOR_EMAIL    = "${data.coder_workspace.me.owner_email}"
-    GIT_COMMITTER_EMAIL = "${data.coder_workspace.me.owner_email}"
-  }
+  #   # install and start code-server
+  #   curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.11.0
+  #   /tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
+  # EOT
 
   # The following metadata blocks are optional. They are used to display
   # information about your workspace in the dashboard. You can remove them
@@ -125,112 +216,239 @@ resource "coder_agent" "main" {
     interval = 60
     timeout  = 1
   }
-
+  
   metadata {
-    display_name = "Swap Usage (Host)"
-    key          = "7_swap_host"
-    script       = <<EOT
-      free -b | awk '/^Swap/ { printf("%.1f/%.1f", $3/1024.0/1024.0/1024.0, $2/1024.0/1024.0/1024.0) }'
+    display_name = "Weather"
+    key  = "weather"
+    # for more info: https://github.com/chubin/wttr.in
+    script = <<EOT
+        curl -s 'wttr.in/seul?format=3' 2>&1 | awk '{print}'
     EOT
-    interval     = 10
-    timeout      = 1
+    interval = 600
+    timeout = 10
   }
+
+  startup_script_behavior = "blocking"
+  startup_script = <<EOT
+#!/bin/bash
+
+# install and start code-server
+curl -fsSL https://code-server.dev/install.sh | sh -s -- --method=standalone --prefix=/tmp/code-server --version 4.11.0
+/tmp/code-server/bin/code-server --auth none --port 13337 >/tmp/code-server.log 2>&1 &
+
+# clone repo
+if test -z "${data.coder_parameter.repo.value}" 
+then
+  echo "No git repo specified, skipping"
+else
+  if [[ ! -d "${local.folder_name}" ]] 
+  then
+    echo "Cloning git repo..."
+    git clone ${data.coder_parameter.repo.value}
+  else
+    echo "Repo ${data.coder_parameter.repo.value} already exists. Will not reclone"
+  fi
+  cd ${local.folder_name}
+fi
+
+# install oh-my-zsh from container image
+curl -fsSL https://raw.githubusercontent.com/taking/taking/main/mySettings/scripts/ohmyzsh_install.sh | bash
+
+# install language from container image
+if [[ ${data.coder_parameter.language.value == "python"} ]]; then
+  # pass
+elif [[ ${data.coder_parameter.language.value == "go"} ]]; then  
+  curl -fsSL https://raw.githubusercontent.com/taking/taking/main/mySettings/scripts/go_install.sh | bash
+  go install -v golang.org/x/tools/gopls@latest
+elif [[ ${data.coder_parameter.language.value == "rust"} ]]; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+elif [[ ${data.coder_parameter.language.value == "java"} ]]; then
+  curl -fsSL https://raw.githubusercontent.com/taking/taking/main/mySettings/scripts/java21_install.sh | bash
+elif [[ ${data.coder_parameter.language.value == "node"} ]]; then
+  curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s 20.15.1
+fi
+
+# # install language extensions from container image
+# if [[ ${data.coder_parameter.language.value == "copilot"} ]]; then
+#   /tmp/code-server/bin/code-server --install-extension /coder/vsix/GitHub.copilot.vsix
+#   /tmp/code-server/bin/code-server --install-extension /coder/vsix/GitHub.copilot-chat.vsix
+# elif [[ ${data.coder_parameter.language.value == "aws"} ]]; then
+#   /tmp/code-server/bin/code-server --install-extension /coder/vsix/AmazonWebServices.aws-toolkit-vscode.vsix
+# elif [[ ${data.coder_parameter.language.value == "tabnine"} ]]; then
+#   /tmp/code-server/bin/code-server --install-extension /coder/vsix/TabNine.tabnine-vscode.vsix
+# fi
+  EOT  
 }
 
+# code-server
 resource "coder_app" "code-server" {
   agent_id     = coder_agent.main.id
   slug         = "code-server"
   display_name = "code-server"
-  url          = "http://localhost:13337/?folder=/home/${local.username}"
   icon         = "/icon/code.svg"
+  url          = "http://localhost:13337?folder=/home/coder"
   subdomain    = false
   share        = "owner"
 
   healthcheck {
     url       = "http://localhost:13337/healthz"
-    interval  = 5
-    threshold = 6
+    interval  = 3
+    threshold = 10
   }
 }
 
-resource "docker_volume" "home_volume" {
-  name = "coder-${data.coder_workspace.me.id}-home"
-  # Protect the volume from being deleted due to changes in attributes.
-  lifecycle {
-    ignore_changes = all
-  }
-  # Add labels in Docker to keep track of orphan resources.
-  labels {
-    label = "coder.owner"
-    value = data.coder_workspace.me.owner
-  }
-  labels {
-    label = "coder.owner_id"
-    value = data.coder_workspace.me.owner_id
-  }
-  labels {
-    label = "coder.workspace_id"
-    value = data.coder_workspace.me.id
-  }
-  # This field becomes outdated if the workspace is renamed but can
-  # be useful for debugging or cleaning out dangling volumes.
-  labels {
-    label = "coder.workspace_name_at_creation"
-    value = data.coder_workspace.me.name
-  }
-}
-
-resource "docker_image" "main" {
-  name = "coder-${data.coder_workspace.me.id}"
-  build {
-    context = "./build"
-    build_args = {
-      USER = local.username
+resource "kubernetes_persistent_volume_claim" "home" {
+  metadata {
+    name      = "coder-${lower(data.coder_workspace_owner.me.name)}-${lower(data.coder_workspace.me.name)}-home"
+    namespace = var.namespace
+    labels = {
+      "app.kubernetes.io/name"     = "coder-pvc"
+      "app.kubernetes.io/instance" = "coder-pvc-${lower(data.coder_workspace_owner.me.name)}-${lower(data.coder_workspace.me.name)}"
+      "app.kubernetes.io/part-of"  = "coder"
+      // Coder specific labels.
+      "com.coder.resource"       = "true"
+      "com.coder.workspace.id"   = data.coder_workspace.me.id
+      "com.coder.workspace.name" = data.coder_workspace.me.name
+      "com.coder.user.id"        = data.coder_workspace_owner.me.id
+      "com.coder.user.username"  = data.coder_workspace_owner.me.name
+    }
+    annotations = {
+      "com.coder.user.email" = data.coder_workspace_owner.me.email
     }
   }
-  triggers = {
-    dir_sha1 = sha1(join("", [for f in fileset(path.module, "build/*") : filesha1(f)]))
+  wait_until_bound = false
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "${data.coder_parameter.home_disk_size.value}Gi"
+      }
+    }
   }
-
-  # Keep alive for other workspaces to use upon deletion
-  keep_locally = true
 }
 
-resource "docker_container" "workspace" {
+resource "coder_metadata" "workspace_info" {
+  count       = data.coder_workspace.me.start_count
+  resource_id = kubernetes_deployment.main[0].id
+  item {
+    key   = "image"
+    value = "codercom/enterprise-base:ubuntu"
+  }
+  item {
+    key   = "language"
+    value = "${data.coder_parameter.language.value}"
+  }
+  item {
+    key   = "repo cloned"
+    value = "${local.repo_owner_name}/${local.folder_name}"
+  }   
+}
+
+resource "kubernetes_deployment" "main" {
   count = data.coder_workspace.me.start_count
-  image = docker_image.main.name
-  # Uses lower() to avoid Docker restriction on container names.
-  name = "coder-${data.coder_workspace.me.owner}-${lower(data.coder_workspace.me.name)}"
-  # Hostname makes the shell more user friendly: coder@my-workspace:~$
-  hostname = data.coder_workspace.me.name
-  dns      = ["8.8.8.8"]
-  # Use the docker gateway if the access URL is 127.0.0.1
-  entrypoint = ["sh", "-c", replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")]
-  env        = ["CODER_AGENT_TOKEN=${coder_agent.main.token}"]
-  host {
-    host = "host.docker.internal"
-    ip   = "host-gateway"
+  depends_on = [
+    kubernetes_persistent_volume_claim.home
+  ]
+  wait_for_rollout = false
+  metadata {
+    name      = "coder-${lower(data.coder_workspace_owner.me.name)}-${lower(data.coder_workspace.me.name)}"
+    namespace = var.namespace
+    labels = {
+      "app.kubernetes.io/name"     = "coder-workspace"
+      "app.kubernetes.io/instance" = "coder-workspace-${lower(data.coder_workspace_owner.me.name)}-${lower(data.coder_workspace.me.name)}"
+      "app.kubernetes.io/part-of"  = "coder"
+      "com.coder.resource"         = "true"
+      "com.coder.workspace.id"     = data.coder_workspace.me.id
+      "com.coder.workspace.name"   = data.coder_workspace.me.name
+      "com.coder.user.id"          = data.coder_workspace_owner.me.id
+      "com.coder.user.username"    = data.coder_workspace_owner.me.name
+    }
+    annotations = {
+      "com.coder.user.email" = data.coder_workspace_owner.me.email
+    }
   }
-  volumes {
-    container_path = "/home/${local.username}"
-    volume_name    = docker_volume.home_volume.name
-    read_only      = false
-  }
-  # Add labels in Docker to keep track of orphan resources.
-  labels {
-    label = "coder.owner"
-    value = data.coder_workspace.me.owner
-  }
-  labels {
-    label = "coder.owner_id"
-    value = data.coder_workspace.me.owner_id
-  }
-  labels {
-    label = "coder.workspace_id"
-    value = data.coder_workspace.me.id
-  }
-  labels {
-    label = "coder.workspace_name"
-    value = data.coder_workspace.me.name
+
+  
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        "app.kubernetes.io/name" = "coder-workspace"
+      }
+    }
+    strategy {
+      type = "Recreate"
+    }
+
+    template {
+      metadata {
+        labels = {
+          "app.kubernetes.io/name" = "coder-workspace"
+        }
+      }
+      spec {
+        security_context {
+          run_as_user = 1000
+          fs_group    = 1000
+        }
+
+        container {
+          name    = "coder-container"
+          image   = "codercom/enterprise-base:ubuntu"
+          image_pull_policy = "Always"
+          command           = ["sh", "-c", coder_agent.main.init_script]
+          security_context {
+            run_as_user = "1000"
+          }
+          env {
+            name  = "CODER_AGENT_TOKEN"
+            value = coder_agent.main.token
+          }
+          resources {
+            requests = {
+              "cpu"    = "250m"
+              "memory" = "512Mi"
+            }
+            limits = {
+              "cpu"    = "${data.coder_parameter.cpu.value}"
+              "memory" = "${data.coder_parameter.memory.value}Gi"
+            }
+          }
+          volume_mount {
+            mount_path = "/home/coder"
+            name       = "home"
+            read_only  = false
+          }
+        }
+
+        volume {
+          name = "home"
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.home.metadata.0.name
+            read_only  = false
+          }
+        }
+
+        affinity {
+          // This affinity attempts to spread out all workspace pods evenly across
+          // nodes.
+          pod_anti_affinity {
+            preferred_during_scheduling_ignored_during_execution {
+              weight = 1
+              pod_affinity_term {
+                topology_key = "kubernetes.io/hostname"
+                label_selector {
+                  match_expressions {
+                    key      = "app.kubernetes.io/name"
+                    operator = "In"
+                    values   = ["coder-workspace"]
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
